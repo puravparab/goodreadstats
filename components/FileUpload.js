317e140681e4c97e0c642eae9bbbb295717c4f82
PuravParab <47409476+PuravParab@users.noreply.github.com>
2023-03-06 23:46:39 +0400

    .csv is added to local storage

--- components/FileUpload.js
+++ components/FileUpload.js
@@ -4,31 +4,72 @@ import styles from '../styles/FileUpload.module.css'
 
 const FileUpload = () => {
 	const [rawFile, setRawFile] = useState('')
-	const [fileData, setFileData] = useState('')
-	const [fileRender, setFileRender] = useState('')
+	// const [fileData, setFileData] = useState('')
+	// const [fileRender, setFileRender] = useState('')
 
-	useEffect(() => {
-    if (fileData) {
-      setFileRender(fileData.split('\n').map((line, index) => {
-        return (
-        	<div key={index} className={styles.fileItem}>
-          	{line}
-        	</div> 
-        )
-      }))
-    }
-  }, [fileData])
+	// useEffect(() => {
+ //    if (fileData) {
+ //    	const rowList = fileData.split('\n')
+ //      setFileRender(rowList.map((line, index) => {
+ //      	const cols = line.split(',')
+ //        return (
+ //        	<div key={index} className={styles.fileRow}>
+ //        		{cols.map((col, i) => {
+ //        			return (
+ //        				(isIncluded(i, rowList[0].split(',')) ? 
+	//         				<div key={i} className={styles.fileCol}>
+	// 	        				{col}
+	// 	        			</div>
+	// 	        			: ""
+ //        				)
+ //        			)
+ //        		})}
+ //        	</div>
+ //        )
+ //      }))
+ //    }
+ //  }, [fileData])
 
+	// Is column included?
+	const isIncluded = (col_index, header) => {
+		const excluded_list = ['Author l-f', 'ISBN13', 'Binding', 'Year Published', 'Bookshelves with positions', 'Exclusive Shelf', 'My Review', 'Spoiler', 'Private Notes', 'Read Count', 'Owned Copies']
+		for (let i = 0; i < excluded_list.length; i++){
+			if (header[col_index] == excluded_list[i]){
+				return false
+			}
+		}
+		return true
+	}
+
+	// When user selects a .csv file
 	const handleFileInputChange = (e) =>{
 		const file = e.target.files[0]
 		setRawFile(file)
 	}
 
+	// When user clicks on the upload button
 	const handleFileUpload = () => {
 		const reader = new FileReader()
 		reader.readAsText(rawFile)
+
 		reader.onload = (e) => {
-			setFileData(e.target.result)
+			// setFileData(e.target.result)
+			const csv_data = e.target.result
+			const rows = csv_data.split('\n')
+			const headers = rows[0].split(',')
+
+			const jsonData = []
+
+			for (let i=1; i < rows.length; i++){
+				const row = rows[i].split(',')
+				const item = {}
+				for (let j=0; j < headers.length; j++){
+					item[headers[j]] = row[j]
+				}
+				jsonData.push(item)
+			}
+
+			localStorage.setItem('goodreads_data', JSON.stringify(jsonData))
 		}
 	}
 
@@ -45,12 +86,7 @@ const FileUpload = () => {
 				 <button onClick={handleFileUpload}>
 				 	Upload
 				 </button>
-			</div>
-
-			<div className={styles.fileContent}>
-				{fileRender}
-			</div>
-			
+			</div>		
 		</div>
 	)
 }
