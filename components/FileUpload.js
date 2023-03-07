923e9857d2ae5ec5de42bd6be7844060f14eacbe
PuravParab <47409476+PuravParab@users.noreply.github.com>
2023-03-08 03:32:13 +0400

    added file validation + layout update

--- components/FileUpload.js
+++ components/FileUpload.js
@@ -6,6 +6,7 @@ import styles from '../styles/FileUpload.module.css'
 
 const FileUpload = () => {
 	const [rawFile, setRawFile] = useState('')
+	const [invalidData, setInvalidData] = useState(false)
 
 	// When user selects a .csv file
 	const handleFileInputChange = (e) =>{
@@ -19,12 +20,30 @@ const FileUpload = () => {
 		Papa.parse(rawFile, {
 			complete: (results) => {
 				rows = results.data;
-				localStorage.setItem('goodreads_data', JSON.stringify(rows))
-				window.dispatchEvent(new Event("storage"));
+				if (validate_csv(rows)){
+					localStorage.setItem('goodreads_data', JSON.stringify(rows))
+					setInvalidData(false)
+					window.dispatchEvent(new Event("storage"))
+				} else{
+					setInvalidData(true)
+				}
 			},
 			header: true,
 			skipEmptyLines: true,
 		})
+	}
+
+	// Validate if data is exported from goodreads
+	const validate_csv = (rows) => {
+		const categories = ['Book Id', 'Title', 'Author', 'Additional Authors', 'ISBN',	'ISBN13',	'My Rating', 'Average Rating',	'Publisher', 'Number of Pages',	'Original Publication Year', 'Date Read',	'Date Added']
+		for (let i=0; i < categories.length; i++){
+			if (rows[0][categories[i]]){
+				continue
+			} else{
+				return false
+			}
+		}
+		return true
 	}
 
 	return (
@@ -37,8 +56,13 @@ const FileUpload = () => {
 
 			<div className={styles.fileInput}>
 				 <input type="file" accept=".csv" onChange={handleFileInputChange}/>
+				 
+				 {invalidData ? 
+					<p>Invalid File!</p> : ""
+				 }
+
 				 <button onClick={handleFileUpload}>
-				 	Upload
+					Upload
 				 </button>
 			</div>		
 		</div>
