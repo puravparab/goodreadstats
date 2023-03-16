66c79358c4ec91411a71688ad4dd649535c2d841
PuravParab <47409476+PuravParab@users.noreply.github.com>
2023-03-16 07:24:22 +0400

    added author chart 2: number of boosk read per author

--- components/Charts/AuthorCharts.js
+++ components/Charts/AuthorCharts.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import 'chart.js/auto'
 import 'chartjs-adapter-moment';
-import { Scatter } from 'react-chartjs-2';
+import { Scatter, Bar } from 'react-chartjs-2';
 
 import styles from '../../styles/Charts.module.css'
 
@@ -9,6 +9,9 @@ import styles from '../../styles/Charts.module.css'
 // 
 // Chart 1: 
 // 	- Scatter plot showing the average ratings the user has given each author compared to the number of books read
+// 	
+// Chart 2:
+// 	- Horizontal bar chart showing number of books read per author
 
 const AuthorCharts = () => {
 	const [detailsVisible, setDetailsVisible] = useState(false)
@@ -17,18 +20,26 @@ const AuthorCharts = () => {
 	const [data1, setData1] = useState({})
 	const [options1, setOptions1] = useState({})
 
+	// Number of books read per author
+	const [data2, setData2] = useState({})
+	const [options2, setOptions2] = useState({})
+
 	useEffect(() => {
 		window.addEventListener('storage', () => {
 			const data = JSON.parse(localStorage.getItem('goodreads_data'))
-			const authorData = getAuthors(data)
-			createChart1(authorData)
+
+			const authorRatings = getAuthorRatings(data)
+			createChart1(authorRatings)
+
+			createChart2(data)
+
 			setDetailsVisible(true)
 		})
 	}, [])
 
 	// Get a json containing data specific to the authors of read books
-	const getAuthors = (data) => {
-		let authorData = {}
+	const getAuthorRatings = (data) => {
+		let authorRatings = {}
 
 		for (let i=0; i<data.length; i++){
 			// If book has been read
@@ -40,14 +51,14 @@ const AuthorCharts = () => {
 				// If user has rated the book
 				if (rating && rating > 0){
 					// If author entry exists in authorData
-					if (authorData[author]){
-						authorData[author]["book_list"].push({
+					if (authorRatings[author]){
+						authorRatings[author]["book_list"].push({
 							title: book,
 							rating: rating
 						})
-						authorData[author]["num_books"] += 1
+						authorRatings[author]["num_books"] += 1
 					} else{
-						authorData[author] = {
+						authorRatings[author] = {
 							book_list: [{
 								title: book,
 								rating: rating
@@ -56,10 +67,10 @@ const AuthorCharts = () => {
 						}
 					}
 				}
+
 			}
 		}
-
-		return authorData
+		return authorRatings
 	}
 
 	// Chart 1:
@@ -110,7 +121,7 @@ const AuthorCharts = () => {
 				y: {
 					title: {
 						display: true,
-						text: 'Number of books read'
+						text: 'Number of books rated'
 					},
 					min: 0,
 					ticks: {
@@ -122,7 +133,7 @@ const AuthorCharts = () => {
 			plugins: {
 				title: {
 					display: true,
-					text: 'Average ratings of author vs number of books read',
+					text: 'Average ratings of author vs number of books rated',
 					font: { size: 16 },
 				},
 				tooltip: {
@@ -135,14 +146,87 @@ const AuthorCharts = () => {
 							return [
 								author,
 								"Average rating: " + avg_rating,
-								"Number of books read: " + num_books
+								"Number of books rated: " + num_books
 							]
 						}
 					}
 				}
 			},
 		})
+	}
 
+	// Chart 2:
+	// 	- Horizontal bar chart showing number of books read per author
+	const createChart2 = (data) => {
+		let authorData = []
+		for (let i=0; i<data.length; i++){
+			// If book has been read
+			if (data[i]["Exclusive Shelf"] == 'read'){
+				const author = data[i]["Author"]
+				if (authorData[author]){
+					authorData[author] += 1
+				} else{
+					authorData[author] = 1
+				}
+			}
+		}
+
+		// Sort the list of authors wrt numbe rof books read
+		const sortedAuthors = Object.keys(authorData).sort((a, b) => authorData[b] - authorData[a])
+		// Display top 
+		const topAuthors = sortedAuthors.slice(0, 23)
+
+		setData2({
+			labels: topAuthors,
+			datasets: [{
+				label: "Authors",
+				data: topAuthors.map(author => authorData[author]),
+				backgroundColor: "rgb(102,187,106)",
+			}]
+		})
+
+		setOptions2({
+			aspectRatio: 1,
+			indexAxis: 'y',
+			scales: {
+				x: {
+					title: {
+						display: true,
+						text: 'Number of books read'
+					},
+					beginAtZero: true,
+				},
+				y: {
+					title: {
+						display: true,
+						text: 'Authors read'
+					}
+				}
+			},
+			plugins: {
+				title: {
+					display: true,
+					text: 'Number of books read per author',
+					font: { size: 16 },
+				},
+				legend: {
+					display: false
+				},
+				tooltip: {
+					callbacks: {
+						title: function(tooltipItem) {
+							const dataIndex = tooltipItem[0].dataIndex
+							const author = tooltipItem[0]["label"]
+							const num_books = tooltipItem[0].dataset.data[dataIndex]
+							return author + ": " + num_books + " books"
+						},
+						label: function(tooltipItem){
+							return ""
+						}
+					}
+				}
+			},
+		})
 	}
 
 	return (
@@ -154,6 +238,9 @@ const AuthorCharts = () => {
 				<div className={styles.bookChartsMain}>
 					<div className={styles.chart}>
 						<Scatter options={options1} data={data1}/>
+					</div>
+					<div className={styles.chart}>
+						<Bar options={options2} data={data2}/>
 					</div>
 				</div>
 				: ""
